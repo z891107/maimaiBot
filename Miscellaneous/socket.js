b6ece707d1496d3778ba00887176bf1a8fd8aa45
unknown <z26062015@gmail.com>
2022-09-17 02:30:34 +0800

    add(Miscellaneous/socket.js)

---
+++ Miscellaneous/socket.js
@@ -0,0 +1,18 @@
+import net from 'net';
+
+var socket = {};
+
+socket.start = () => {
+    var server = net.createServer();
+
+    server.listen(9999, function () {
+        console.log('TCP Server start');
+    });
+
+    server.on('connection', function () {
+        console.log('TCP get connection')
+    })
+};
+
+export default socket;
+
--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,14 @@
+import socket from '../Miscellaneous/socket.js'
 import maimaiScoreTracer from '../maimai/app.js';
 
 export default {
 	name: 'ready',
 	once: true,
 	execute(client) {
-        console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log(`Ready! Logged in as ${client.user.tag}`);
 
-        maimaiScoreTracer.start(client);
+        socket.start();
+
+		maimaiScoreTracer.start(client);
 	},
 };
